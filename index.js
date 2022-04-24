71763a46ae6a567d9863575eac11caabb5d862fc
unknown <mehankjain1670@gmail.com>
2022-04-25 01:51:46 +0530

    url changes

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://thriving-beignet-91f203.netlify.app/",
+    ],
     credentials: true,
   })
 );
